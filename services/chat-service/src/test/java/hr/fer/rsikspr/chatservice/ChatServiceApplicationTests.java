ccb68029ad81b923af132c7dc4c5c987ed670072
Ivan Jeržabek <jerzabek.ivan@gmail.com>
2023-06-24 19:35:13 +0200

    Cleanup container build

---
+++ services/chat-service/src/test/java/hr/fer/rsikspr/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package hr.fer.rsikspr.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChatServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
